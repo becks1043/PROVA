c5c09c1a9fb31d5897a3395b296ee263f431ce58
emilia boschi <emilia.boschi@gmail.com>
2024-10-03 18:17:18 +0200

    implementato la velocità della particella in Prticle

--- task2.py
+++ task2.py
@@ -1,4 +1,5 @@
 import math
+import scipy.constants as constants
 
 #Write a program to explore the properties of a few elementary Particles.
 class Particle: #le classi sempre in MAIUSC
@@ -10,15 +11,23 @@ class Particle: #le classi sempre in MAIUSC
         self.momentum = momentum #impulso in MeV/c
 
     def energy(self):
-        return math.sqrt(self.momentum**2 + self.mass**2)
+        return math.sqrt(self.momentum**2 + (self.mass*constants.c)**2)*constants.c #MeV
+    
+    def velocity(self):
+        return (self.momentum*constants.c**2)/ self.energy()
 
 class Electron(Particle):
     def __init__(self , momentum = 0): #perchè il momento deve essere =0?
         Particle.__init__(self, 0.511, 'e-', 'elettrone', momentum)
 
+class Proton(Particle):
+    def __init__(self, momentum = 0):
+        Particle.__init__(self, 981, 'e+','protone', momentum)
 
 el = Electron(momentum= 10)
-print("L'energia del  {} è {}".format(el.name, el.energy()))
+pt = Proton(momentum = 10)
+print("L'energia del  {} è {}. La sua velocità {}".format(el.name, el.energy(), el.velocity()))
+print("L'energia del  {} è {}. La sua velocità {}".format(pt.name, pt.energy(),pt.velocity()))
 
 
     
